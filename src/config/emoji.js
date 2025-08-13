ef077b1a309bc620dfd3ef8c614efc386feb418d
Priyanshuuuu <167881010+bre4d777@users.noreply.github.com>
2025-08-13 15:01:32 +0530

    Update emoji.js

--- src/config/emoji.js
+++ src/config/emoji.js
@@ -1,9 +1,13 @@
 export const emoji   ={
+
   "check": "<:yuki_check:1404750323817517126>",
-  "info":"<:yuki_info:1404750536401616988>",
-  "cross":"<:yuki_cross:1404750742249541762>",
+  "info": "<:yuki_info:1404750536401616988>",
+  "cross": "<:yuki_cross:1404750742249541762>",
   "add": "<:yuki_add:1405044085076791336>",
   "reset": "<:yuki_reset:1405115815241453681>",
+  "folder": "<:yuki_folder:1405120584232337470>",
+  "openfolder": "<:yuki_openfolder:1405120588908855371>",
+
   get(name, fallback   ='') {
     return this[name] || fallback;
   },
